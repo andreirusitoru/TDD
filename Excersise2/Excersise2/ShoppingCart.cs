f26d2280b650f48ad94d55de7984d84332a858cf
Andrei Rusitoru <andrei.rusitoru@red-gate.com>
2017-03-10 14:01:27 +0000

    Exercise2

---
+++ Excersise2/Excersise2/ShoppingCart.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersise2
+{
+    public class ShoppingCart
+    {
+        private List<Discount> _discounts = new List<Discount>();
+        private List<Item> _items = new List<Item>();
+
+        public decimal Total => CalculateTotal();
+
+        public void AddDiscount(decimal minimumAmount, decimal discountPercentage)
+        {
+            _discounts.Add(new Discount(minimumAmount, discountPercentage));
+        }
+
+        public void AddItem(decimal price, int quantity)
+        {
+            _items.Add(new Item(price, quantity));
+        }
+
+        private decimal CalculateTotal()
+        {
+            var total = _items.Sum(item => item.Price * item.Quantity);
+            var discountToApply =
+                _discounts.Where(discount => discount.MinimumAmount < total)
+                    .OrderByDescending(discount => discount.MinimumAmount)
+                    .FirstOrDefault();
+
+            if (discountToApply != null)
+            {
+                total -= total * discountToApply.DiscountPercentage / 100;
+            }
+
+            return total;
+        }
+    }
+
+    internal class Item
+    {
+        public decimal Price { get; }
+
+        public int Quantity { get; }
+
+        public Item(decimal price, int quantity)
+        {
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+
+    internal class Discount
+    {
+        public decimal MinimumAmount { get; }
+        public decimal DiscountPercentage { get; }
+
+        public Discount(decimal minimumAmount, decimal discount)
+        {
+            MinimumAmount = minimumAmount;
+            DiscountPercentage = discount;
+        }
+    }
+}
---
+++ Excersise2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Excersise2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldBuyMore()
+        {
+            var shoppingCart = new ShoppingCart();
+
+            shoppingCart.AddItem(50, 2);
+
+            Assert.AreEqual(shoppingCart.Total, 100);
+        }
+
+        [TestMethod]
+        public void ShouldGiveSomeDiscount()
+        {
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.AddDiscount(100, 5);
+            shoppingCart.AddDiscount(200, 10);
+
+            shoppingCart.AddItem(70, 2);
+            shoppingCart.AddItem(20, 2);
+
+            Assert.AreEqual(shoppingCart.Total, 171);
+        }
+
+        [TestMethod]
+        public void ShouldGiveMoreDiscount()
+        {
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.AddDiscount(100, 5);
+            shoppingCart.AddDiscount(200, 10);
+
+            shoppingCart.AddItem(70, 2);
+            shoppingCart.AddItem(50, 2);
+
+            Assert.AreEqual(shoppingCart.Total, 216);
+        }
+    }
+}
